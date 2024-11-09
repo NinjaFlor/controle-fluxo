b6c6f986b8e6a9f15cc7798fec591a179296458a
NinjaFlor <[200~Repository>
2024-11-09 11:33:29 -0300

    Utilizando funão ternária para substituir if else encadeado

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,16 +1,25 @@
 public class ResultadoEscolar {
 
     public static void main(String[] args) {
-        int nota = 4;
+        // no lugar de utiliza if else encadeado, utilizamos uma expressão ternária que
+        // joga para uma variável. Através dessa váriável é possível imprimir, salvar em
+        // arquivo....
 
-        if (nota >= 7)
-            System.out.println("Aprovado");
+        int nota = 8;
+        // String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
 
-        else if (nota >= 5 && nota < 7)
-            System.out.println("Prova de Recuperação");
+        // Estrutura de teste encadeado. Estrutura removiva para teste de Condição
+        // Ternária.
+        // if (nota >= 7)
+        // System.out.println("Aprovado");
 
-        else
-            System.out.println("Reprovado");
+        // else if (nota >= 5 && nota < 7)
+        // System.out.println("Prova de Recuperação");
+
+        // else
+        // System.out.println("Reprovado");
 
     }
 
