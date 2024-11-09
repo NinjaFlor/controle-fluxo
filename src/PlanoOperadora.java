87f29e624f164bb093f80fc67a637ff1efcb3076
NinjaFlor <andreiac.lima@outlook.com.br>
2024-11-09 13:12:12 -0300

    Uso mais apropriado para o switch case

---
+++ src/PlanoOperadora.java
@@ -0,0 +1,18 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+//switch case é melhor para algo complementar, e não para controles de fluxos condicionais, mesmo que seja encadeado.
+        switch (plano) {
+            case "T": {
+                System.out.println("5G Youtube");
+            }
+            case "M": {
+                System.out.println("Whats e Instagram grátis");
+            }
+            case "B": {
+                System.out.println("100 minutos de ligação");
+            }
+        }
+    }
+
+}
